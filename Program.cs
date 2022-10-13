3bd8ac27d7482539f9d048a33801f0c6475cc399
jayukasode23 <jayukasode2398@gmail.com>
2022-10-13 20:51:48 +0530

    Added Employee Wage Program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using EmployeeWage4Program;
+Console.WriteLine("Welcome To Employee Wage Computation Program");
+
+EmployeeCheck obj = new EmployeeCheck();
+obj.CheckEmployeeAttendance();
+
+DailyEmployeeWage obj1 = new DailyEmployeeWage();
+obj1.CheckDailyEmployeeWage();
+
+PartTimeWage obj2 = new PartTimeWage();
+obj2.CheckEmployeePartTimeWage();
+
+SwitchCaseStatement obj3 = new SwitchCaseStatement();
+obj3.CheckDailyEmpWage();
+
+EmployeeMonthWages obj4 = new EmployeeMonthWages();
+obj4.CheckEmployeeMonthlyWage();
+
+EmployeeTotalWages obj5 = new EmployeeTotalWages();
+obj5.CheckEmployeeMonth();
+
+RefactoreEmployeeWage employee = new RefactoreEmployeeWage();
+employee.CheckComputeEmployeeWage();
+
+EmpWageMultipleCompany empWageMultipleCompany = new EmpWageMultipleCompany();
+empWageMultipleCompany.computeEmpWage("DMart",20,2,10);
+empWageMultipleCompany.computeEmpWage("Jio", 25, 5, 20);
